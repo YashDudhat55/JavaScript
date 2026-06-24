f49f772fb5a43688cbb4b56be6019f1ed045a582
Yash Dudhat <yashrd2009@gmail.com>
2026-06-24 13:35:19 +0530

    JavaScript

---
+++ CH-2/js/script.js
@@ -0,0 +1,110 @@
+console.log("Welcome to JavaScript");
+console.log("");
+
+console.log("LW - 1.2");
+console.log("");
+
+console.log("Name : Yash Dudhat");
+console.log("Age : 17");
+console.log("School : Red & White\n");
+console.log("");
+
+console.log("----------------");
+console.log("|               |");
+console.log("R               |");
+console.log("N               |");
+console.log("W               |");
+console.log("|               |");
+console.log("----------------");
+console.log("");
+
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+console.log("");
+
+console.log("*");
+console.log("*");
+console.log("*");
+console.log("*      * *       *");
+console.log("*     *   *     *");
+console.log("*    *     *   *");
+console.log("*   *       * *");
+console.log("*  *");
+console.log("* *");
+console.log("*");
+console.log("");
+
+
+console.log("LW - 2.1");
+console.log("");
+
+console.log(`A = ${50} B = ${10} Addition = ${50 + 10} `);
+console.log(`A = ${50} B = ${10} Subtraction = ${50 - 10} `);
+console.log(`A = ${50} B = ${10} Multiplication = ${50 * 10} `);
+console.log(`A = ${50} B = ${10} Division = ${50 / 10} `);
+console.log(`A = ${50} B = ${10} Modulus = ${50 % 10} `);
+console.log("");
+
+
+
+console.log("----------------");
+console.log("|               |");
+console.log("|  5 * 5  = 25  |");
+console.log("|               |");
+console.log("----------------");
+
+// Main Page
+
+document.write("Welcome to JavaScript<br><br>");
+
+document.write("LW - 1.2<br><br>");
+
+document.write("Name : Yash Dudhat<br>");
+document.write("Age : 17<br>");
+document.write("School : Red & White<br><br>");
+
+document.write("----------------<br>");
+document.write("|               |<br>");
+document.write("R               |<br>");
+document.write("N               |<br>");
+document.write("W               |<br>");
+document.write("|               |<br>");
+document.write("----------------<br><br>");
+
+document.write("*<br>");
+document.write("**<br>");
+document.write("***<br>");
+document.write("****<br>");
+document.write("***<br>");
+document.write("**<br>");
+document.write("*<br><br>");
+
+document.write("*<br>");
+document.write("*<br>");
+document.write("*<br>");
+document.write("*      * *       *<br>");
+document.write("*     *   *     *<br>");
+document.write("*    *     *   *<br>");
+document.write("*   *       * *<br>");
+document.write("*  *<br>");
+document.write("* *<br>");
+document.write("*<br><br>");
+
+document.write("LW - 2.1<br><br>");
+
+document.write(`A = 50 B = 10 Addition = ${50 + 10}<br>`);
+document.write(`A = 50 B = 10 Subtraction = ${50 - 10}<br>`);
+document.write(`A = 50 B = 10 Multiplication = ${50 * 10}<br>`);
+document.write(`A = 50 B = 10 Division = ${50 / 10}<br>`);
+document.write(`A = 50 B = 10 Modulus = ${50 % 10}<br><br>`);
+
+document.write("----------------<br>");
+document.write("|               |<br>");
+document.write("|  5 * 5 = 25   |<br>");
+document.write("|               |<br>");
+document.write("----------------<br>");
---
+++ CH-2/js/user.js
@@ -0,0 +1,14 @@
+let name = prompt("Enter your name:");
+let age = parseFloat(prompt("Enter your age:"));
+let height = parseFloat(prompt("Enter your height in cm:"));
+
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("Height : " + height + " cm");
+console.log("Data type of name : " + typeof name);
+console.log("Data type of age : " + typeof age);
+console.log("Data type of height : " + typeof height);
+
+console.log("");
+let radius = parseFloat(prompt("Enter the radius of a circle:"));
+console.log("Radius : " + radius);
